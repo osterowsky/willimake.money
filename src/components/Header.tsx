976eb3c20347160d9046dcdf58f076b36860193d
osterowsky <ostern.patryk02@gmail.com>
2025-07-05 21:32:11 +0200

    feature: add header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,15 @@
+import './Header.css'
+import './../index.css'
+
 function Header() {
 
   return (
-    <div/>
+    <div id="header">
+      <h1 id="logo">willimake.money</h1>
+      <div className="nav">
+        <h3><a href="#about">about</a></h3>
+      </div>
+    </div>
   )
 }
 
